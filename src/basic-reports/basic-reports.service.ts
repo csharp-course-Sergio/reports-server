f650696af528eb2e2451554395f3fadc075935ac
Sergio Barreras <sergiobg.isc@gmail.com>
2024-05-30 16:05:19 -0700

    configuración de pdf make package

--- src/basic-reports/basic-reports.service.ts
+++ src/basic-reports/basic-reports.service.ts
@@ -1,13 +1,32 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
 
+import PdfPrinter from 'pdfmake';
+import type { TDocumentDefinitions } from 'pdfmake/interfaces';
+
+const fonts = {
+  Roboto: {
+    normal: 'fonts/Roboto-Regular.ttf',
+    bold: 'fonts/Roboto-Medium.ttf',
+    italics: 'fonts/Roboto-Italic.ttf',
+    bolditalics: 'fonts/Roboto-MediumItalic.ttf',
+  },
+};
+
 @Injectable()
 export class BasicReportsService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
   }
 
-  async hello() {
-    return this.employees.findFirst();
+  hello() {
+    const printer = new PdfPrinter(fonts);
+
+    const docDefinition: TDocumentDefinitions = {
+      content: 'Hola mundo',
+    };
+
+    const doc = printer.createPdfKitDocument(docDefinition);
+    return doc;
   }
 }
