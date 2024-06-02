67ebc1dba2cbb682d2223df141b4715a3cca07db
Sergio Barreras <sergiobg.isc@gmail.com>
2024-06-01 22:05:30 -0700

    header props

--- src/reports/employment-letter.report.ts
+++ src/reports/employment-letter.report.ts
@@ -1,9 +1,6 @@
-import type {
-  Content,
-  StyleDictionary,
-  TDocumentDefinitions,
-} from 'pdfmake/interfaces';
-import { DateFormatter } from 'src/helpers';
+import type { StyleDictionary, TDocumentDefinitions } from 'pdfmake/interfaces';
+
+import { headerSection } from './sections/header.section';
 
 const style: StyleDictionary = {
   header: {
@@ -28,28 +25,11 @@ const style: StyleDictionary = {
   },
 };
 
-const logo: Content = {
-  image: 'src/assets/tucan-code-logo.png',
-  width: 100,
-  height: 100,
-  alignment: 'center',
-  margin: [0, 0, 0, 20],
-};
-
 export const getEmploymentLetter = (): TDocumentDefinitions => {
   const docDefinition: TDocumentDefinitions = {
     styles: style,
     pageMargins: [40, 60, 40, 60],
-    header: {
-      columns: [
-        logo,
-        {
-          text: DateFormatter.getDDMMMMYYYY(new Date()),
-          alignment: 'right',
-          margin: [20, 20],
-        },
-      ],
-    },
+    header: headerSection({}),
     content: [
       {
         text: `CONSTANCIA DE EMPLEO`,
---
+++ src/reports/sections/header.section.ts
@@ -0,0 +1,34 @@
+import type { Content } from 'pdfmake/interfaces';
+import { DateFormatter } from 'src/helpers';
+
+const logo: Content = {
+  image: 'src/assets/tucan-code-logo.png',
+  width: 100,
+  height: 100,
+  alignment: 'center',
+  margin: [0, 0, 0, 20],
+};
+
+interface HeaderOptions {
+  title?: string;
+  subtitle?: string;
+  showLogo?: boolean;
+  showDate?: boolean;
+}
+
+export const headerSection = (options: HeaderOptions): Content => {
+  const { title, showDate = true, showLogo = true, subtitle } = options;
+
+  const headerLogo: Content = showLogo ? logo : '';
+  const headerDate: Content = showDate
+    ? {
+        text: DateFormatter.getDDMMMMYYYY(new Date()),
+        alignment: 'right',
+        margin: [20, 20],
+      }
+    : '';
+
+  return {
+    columns: [headerLogo, headerDate],
+  };
+};
