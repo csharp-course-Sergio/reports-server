6fe8499c1bd4bd2c4b8f4287115027f9c1d76754
Sergio Barreras <sergiobg.isc@gmail.com>
2024-07-28 19:58:47 -0700

    HTML complejo

--- src/extra-reports/extra-reports.service.ts
+++ src/extra-reports/extra-reports.service.ts
@@ -5,22 +5,31 @@ import { PrinterService } from 'src/printer/printer.service';
 import { getHtmlContent } from 'src/helpers/html-to-pdfmake';
 import type { TDocumentDefinitions } from 'pdfmake/interfaces';
 import { headerSection } from 'src/reports/sections/header.section';
+import { footerSection } from 'src/reports/sections/footer.sections';
 
 @Injectable()
 export class ExtraReportsService {
   constructor(private readonly printerService: PrinterService) {}
 
   getHtmlReport() {
-    const html = fs.readFileSync('src/reports/html/basic-01.html', 'utf8');
-    const content = getHtmlContent(html);
+    const html = fs.readFileSync('src/reports/html/basic-03.html', 'utf8');
+
+    const content = getHtmlContent(html, {
+      client: 'Sergio Barreras',
+      title: 'Curso de NestJS + Reportes',
+    });
+
     const docDefinition: TDocumentDefinitions = {
       pageMargins: [40, 110, 40, 60],
       header: headerSection({
         title: 'HTML to PDFMake',
       }),
+      footer: footerSection,
       content,
     };
+
     const doc = this.printerService.createPdf(docDefinition);
+
     return doc;
   }
 }
